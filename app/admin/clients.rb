ebba0cbaef74c12cae6522b51f2c0f36bfa121ea
Catalin Ionescu <catalin.ionescu282@gmail.com>
2020-12-16 22:33:53 +0200

    Part 2 Chaper 3.1
    Bonus: Make a custom client form to leverage the Active Admin date picker

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,4 +1,16 @@
 ActiveAdmin.register Client do
   permit_params :name, :phone_number, :email, :birthday
 
+  form do |f|
+    f.semantic_errors *f.object.errors.keys
+
+    f.inputs do
+      f.input :name
+      f.input :phone_number
+      f.input :email
+      f.input :birthday, as: :datepicker
+    end
+
+    f.actions
+  end
 end
